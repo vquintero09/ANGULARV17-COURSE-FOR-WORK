b54a5bf30a4d1ff544693f70b7a4d03a51af80dd
Victor Quintero <167581773+vquintero09@users.noreply.github.com>
2024-06-19 17:51:39 -0700

    Agregamos un formArray de formsGroups para controlar dinamicamente la candidad de productos ingresados

--- ANGULARV17-COURSE-FOR-WORK/src/app/pages/payment-page/simple-product-detail/simple-product-detail.component.ts
+++ ANGULARV17-COURSE-FOR-WORK/src/app/pages/payment-page/simple-product-detail/simple-product-detail.component.ts
@@ -1,43 +1,99 @@
-import { Component, inject } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { group } from '@angular/animations';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { MatButton, MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInput, MatInputModule } from '@angular/material/input';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { Router } from '@angular/router';
 
 
-export interface PeriodicElement {
+interface ICartProduct {
 	name: string;
-	position: number;
-	weight: number;
-	symbol: string;
+	price: number;
+	quantity: number;
+	total: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-	{ position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
-	{ position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
-	{ position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
-	{ position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
-	{ position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
-	{ position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
-	{ position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
-	{ position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
-	{ position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
-	{ position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' }
+const PRODUCTS: ICartProduct[] = [
+	{ name: 'Mens Casual Slim Fit', price: 109.95, quantity: 2, total: 219.9 },
+	{ name: "ohn Hardy Women's Legends Naga Gold & Silver Dragon", price: 695, quantity: 1, total: 695 },
+	{ name: 'ierced Owl Rose Gold Plated Stainless Steel Double', price: 10.99, quantity: 1, total: 10.99 }
 ];
 
 @Component({
   selector: 'app-simple-product-detail',
   standalone: true,
-  imports: [MatTableModule],
+  imports: [
+		MatTableModule, 
+		ReactiveFormsModule, 
+		MatInput, 
+		MatFormFieldModule, 
+		MatInputModule,
+		MatIconModule,
+		MatButtonModule
+	],
   templateUrl: './simple-product-detail.component.html',
   styleUrl: './simple-product-detail.component.scss'
 })
-export class SimpleProductDetailComponent {
-	private readonly _router = inject(Router)
+export class SimpleProductDetailComponent implements OnInit{
+	private readonly _router = inject(Router);
+	private readonly _formBuilder = inject(FormBuilder);
+
 	constructor () {
 		console.log(this._router.getCurrentNavigation()?.extras.state); //Obetner los valores pasados por url con data state
 	}
 
+  displayedColumns: string[] = ['name', 'price', 'quantity', 'total', 'action'];
+	// dataSource = PRODUCTS;
+
+
+	form = this._formBuilder.group({
+		total: PRODUCTS.reduce((prev, current) => prev + current.total, 0),
+		products: this._formBuilder.array(PRODUCTS.map((item) => this._crateFormGroup(item)))
+	});
+
+	dataSource = new MatTableDataSource(this.form.controls.products.controls)
+
 	
 
-  displayedColumns: string[] = ['position', 'name', 'weight', 'symbol'];
-	dataSource = ELEMENT_DATA;
+	ngOnInit(): void {
+		this._calculate_row_total();
+	}
+
+	private _crateFormGroup(item: ICartProduct) {
+		return this._formBuilder.group({
+			name: item.name,
+			price: item.price,
+			quantity: item.quantity,
+			total: item.total
+		});
+	};
+
+	private _calculate_row_total() {
+		this.productsFormArray.controls.forEach(({controls: {quantity, price, total}}) => {
+			quantity.valueChanges.subscribe((value) =>{
+				const priceValue = price.value!;
+				let totalValue = 0;
+
+				if(value) {
+					totalValue = priceValue * value
+				}
+
+				total.setValue(totalValue)
+				
+			})
+		})
+	}
+
+	get productsFormArray() {
+		return this.form.controls.products;
+	}
+
+	clickDelete(index: number){
+		this.productsFormArray.removeAt(index);
+
+		this.dataSource.data = this.productsFormArray.controls;
+	}
 }
